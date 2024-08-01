345db6479118a5d9496fde8058a7284eba70fa53
ErickRodPi <erickrodpi@gmail.com>
2024-08-01 15:36:30 -0600

    Update Paso6Tarea1.py
    Paso 7:
    Se arreglaron los errores de Flake 8
    E261
    W293
    E275

--- Tarea 1/Paso6Tarea1.py
+++ Tarea 1/Paso6Tarea1.py
@@ -1,9 +1,9 @@
-def operaciones(n1, n2, ope): # Función de prueba que solo multiplica y divide
-    
+def operaciones(n1, n2, ope):  # Función de prueba que solo multiplica y divide
+
     if ope == "*":
         resultado = n1 * n2
 
     elif ope == "/":
         resultado = n1 / n2
 
-    return(0, resultado)
+    return (0, resultado)
