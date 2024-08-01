c1d1d2e5a2d0f16336cf9d258a6925f2f846a210
ErickRodPi <erickrodpi@gmail.com>
2024-08-01 15:34:29 -0600

    Add files via upload
    Paso 6 Tarea 1. Con 3 errores de Flake 8 
    Errores:
    E261
    W293
    E275

---
+++ Tarea 1/Paso6Tarea1.py
@@ -0,0 +1,9 @@
+def operaciones(n1, n2, ope): # Función de prueba que solo multiplica y divide
+    
+    if ope == "*":
+        resultado = n1 * n2
+
+    elif ope == "/":
+        resultado = n1 / n2
+
+    return(0, resultado)
